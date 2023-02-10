f8afe990de6ecc5c4de9d18273eef27132f6640e
suFi7867 <noorani786.ss@gmail.com>
2023-02-10 16:51:39 +0530

    SERVER CREATED

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
 
 const express = require("express")
+const app = express()
 
+const PORT = process.env.PORT || 3005
 
+app.use("/", (req,res)=>{
+    res.status(200).send("Server Started")
+})
+
+app.listen(PORT , ()=>{
+    console.log(`Server Started on Port ${PORT}`)
+})
 
 
 
